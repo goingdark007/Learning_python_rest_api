d28fc22535fd6eabd7df19c592b92127dbe2fd3d
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2025-11-02 17:11:12 +0600

    Adding a bit of tkinter gui

--- prapi2_currency_converter.py
+++ prapi2_currency_converter.py
@@ -1,27 +1,146 @@
 import requests
+# imports tkinter gui
+import tkinter as tk
+
+# .Tk class creates a root window
+window = tk.Tk()
+
+
+# configures the row & column when its expanded and minimum size is 50 pixel
+window.rowconfigure(0, weight= 1, minsize= 50)
+window.columnconfigure([0, 1, 2], weight= 1, minsize = 50)
 
 url = "https://api.fastforex.io/fetch-one"
-
-
 query_params = {
-
     'api_key' : 'b92b0ae420-a151890833-t53fpj',
     'from'  : 'USD',
     'to' : 'BDT'
-
 }
 
-response = requests.get(url, params= query_params)
+# Global variable to remember which entry was last typed in
+last_edited = None
 
-data = response.json()
+#This function runs whenever a key is pressed inside an Entry. It stores the entry widget
+# (entry or entry_c) in last_edited
+def set_last_edited(event):
+    # global means modifying the global var not creating a local one for function
+    global last_edited
+    # event.widget tells us which widget triggered the event (either entry or entry_c)
+    last_edited = event.widget
 
-convertion_rate = data['result']['BDT']
+def convert(value):
 
-usd_amount = 123
+    global last_edited
 
-print(f'123 dollars is {usd_amount * convertion_rate} Taka')
+    if last_edited == entry_usd:
 
-bdt_amount = 4500
+        f_val = entry_usd.get()
 
-print(f'4500 taka is {bdt_amount / convertion_rate} dollars')
+        #if f_val:
+    
 
+def get_convertion_rate():
+
+    try:
+
+        response = requests.get(url, params=query_params)
+        response.raise_for_status() # Raises an HTTPError for bad codes
+        data = response.json()
+        convertion_rate = data['result']['BDT']
+        convert(convertion_rate)
+
+
+    except requests.exceptions.HTTPError as errh:
+        print("HTTP Error:", errh)
+
+    except requests.exceptions.ConnectionError as errc:
+        print("Connection Error:", errc)
+
+    except requests.exceptions.Timeout as errt:
+        print("Timeout Error:", errt)
+
+    except requests.exceptions.RequestException as err:
+        print("Error:", err)
+
+
+# Creating a list to store the frames
+frmList = []
+
+# Creating a nested loop where i iterates 1 time and j iterates 3 times
+for i in range(1):
+    for j in range(3):
+        #Creating a frame for each iteration of j, in total four frames created
+        frame = tk.Frame(master=window,relief= "ridge", borderwidth=1)
+        # Creating a 1 row/3 column grid and placing the frames in 0,0/0,1/0,2 cells
+        frame.grid(row=i, column=j)
+        # Adding each frames individually in the list
+        frmList.append(frame)
+
+# Creating an entry widget to take the input from user
+entry_usd = tk.Entry(
+    # master= means this widgets parent container is frmList[0] 0th index frame
+    master= frmList[0],
+    width= 10,
+    font= ('Arial', 25),
+    borderwidth= 1
+)
+
+
+# Creating a label widget to show the text `F
+lbl_f = tk.Label(
+    master= frmList[0],
+    text= '`F',
+    height = 5,
+    width = 3
+)
+
+
+# Creating a button widget to tap and start the program
+btn_convert = tk.Button(
+    master= frmList[1],
+    text= '<-->',
+    height = 4,
+    width = 10,
+    command= get_convertion_rate
+)
+
+entry_bdt = tk.Entry(
+    master= frmList[2],
+    width= 10,
+    font= ('Arial', 25),
+    borderwidth= 1
+)
+
+# Creating another label widget to show the output
+lbl_c = tk.Label(
+    master = frmList[2],
+    text = '`C',
+    height = 5,
+    width = 3
+)
+
+# Binds the widgets to event function. <Key> = keyboard sensor: goes off whenever we type
+# something inside the widget and <FocusIn> = attention sensor: goes off when the widget
+# gains the user’s focus (clicked or tabbed into) & lastly it calls the event function which updates the last_edited
+entry_usd.bind("<Key>", set_last_edited)
+entry_bdt.bind("<Key>", set_last_edited)
+entry_usd.bind("<FocusIn>", set_last_edited)
+entry_bdt.bind("<FocusIn>", set_last_edited)
+
+# Placing the widgets in the frame to show up
+# side= tk.LEFT / "left" parameter is used to stack widgets in a frame at the left side by default it stacks at top
+entry_usd.pack(side= "left")
+lbl_f.pack()
+# padx= and pady= to give a lil bit of space between button widget and frame border both horizontally & vertically
+btn_convert.pack(padx= 5, pady= 5)
+entry_bdt.pack(side= 'left')
+lbl_c.pack()
+
+
+# .mainloop() runs the gui
+window.mainloop()
+
+# usd_amount = 123
+# print(f'123 dollars is {usd_amount * convertion_rate} Taka')
+# bdt_amount = 4500
+# print(f'4500 taka is {bdt_amount / convertion_rate} dollars')
